41045ce5b2ecf994549760603bbe0b07f8e5b9c2
DrewGodden <35704776+DrewGodden@users.noreply.github.com>
2019-03-22 00:16:50 -0400

    Allowed checkpoints to be hit in any order via Unity Events and temporarily commented out timing system from checkpoint code.

--- Assets/Bounce Mountain/Scripts/Checkpoint.cs
+++ Assets/Bounce Mountain/Scripts/Checkpoint.cs
@@ -2,38 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class OnHitEvent : UnityEvent { }
 
 public class Checkpoint : MonoBehaviour
 {
+    public OnHitEvent OnHit = new OnHitEvent();
+    private bool hit = false;
+
+    /* Checkpoint time code - removed due to being obsolete
     public int checkpointNumber;
     public Text checkpointTimeText;
     private float timeFromStart;
     private float checkpointReachedTime;
 
+    
     // Update is called once per frame
     void Update()
     {
         timeFromStart = Time.time;
     }
+    */
 
     private void OnTriggerEnter2D(Collider2D trigger)
     {
-        if (trigger.gameObject.tag == "Player")
+        if (trigger.gameObject.tag == "Player" && hit == false)
         {
-            if (trigger.GetComponent<PlayerController>().currentCheckPoint == checkpointNumber - 1)
+            //if (trigger.GetComponent<PlayerController>().currentCheckPoint == checkpointNumber - 1)
+            //{
+            gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+            OnHit.Invoke();
+            Debug.Log("OnHit invoked!");
+            hit = true;
+
+            /* Checkpoint time code - removed due to being obsolete
+            checkpointReachedTime = timeFromStart - trigger.GetComponent<PlayerController>().lastCheckpointTime;
+
+            if (checkpointTimeText != null)
             {
-                checkpointReachedTime = timeFromStart - trigger.GetComponent<PlayerController>().lastCheckpointTime;
-
-                if (checkpointTimeText != null) checkpointTimeText.text = "Checkpoint " + checkpointNumber + ": " + checkpointReachedTime;
-                trigger.GetComponent<PlayerController>().lastCheckpointTime += checkpointReachedTime;
-                trigger.GetComponent<PlayerController>().currentCheckPoint = checkpointNumber;
-                gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+                checkpointTimeText.text = "Checkpoint " + checkpointNumber + ": " + checkpointReachedTime;
             }
-        }
-    }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("Collision!");
+            trigger.GetComponent<PlayerController>().lastCheckpointTime += checkpointReachedTime;
+            trigger.GetComponent<PlayerController>().currentCheckPoint = checkpointNumber;
+            */
+            //}
+        }
     }
 }
--- Assets/Bounce Mountain/Scripts/GameManager.cs
+++ Assets/Bounce Mountain/Scripts/GameManager.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public Checkpoint OnHitEvent;
+    private int checkpointsInLevel;
+    private int checkpointsRemaining;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        checkpointsInLevel = GameObject.FindGameObjectsWithTag("Checkpoint").Length;
+        checkpointsRemaining = checkpointsInLevel;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(checkpointsRemaining == 0)
+        {
+            Debug.Log("Level Complete!");
+        }
+    }
+
+    public void OnCheckpointHit()
+    {
+        checkpointsRemaining -= 1;
     }
 }
