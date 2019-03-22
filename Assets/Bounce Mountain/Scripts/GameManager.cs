837402a1bff0cae7563adef1d5188c5cbcef0e2d
Drew Godden <godden.drew@gmail.com>
2019-03-22 18:44:08 -0400

    Added Main Menu and scene transitions

--- Assets/Bounce Mountain/Scripts/GameManager.cs
+++ Assets/Bounce Mountain/Scripts/GameManager.cs
@@ -1,31 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public Checkpoint OnHitEvent;
     private int checkpointsInLevel;
     private int checkpointsRemaining;
+    private Scene currentScene;
 
     // Start is called before the first frame update
     void Start()
     {
         checkpointsInLevel = GameObject.FindGameObjectsWithTag("Checkpoint").Length;
         checkpointsRemaining = checkpointsInLevel;
+        currentScene = SceneManager.GetActiveScene();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(checkpointsRemaining == 0)
+        if (checkpointsRemaining == 0 && currentScene.name != "sce_Main_Menu")
         {
-            Debug.Log("Level Complete!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 
     public void OnCheckpointHit()
     {
         checkpointsRemaining -= 1;
+    }
+
+    public void OnPlayButtonPressed()
+    {
+        SceneManager.LoadScene("sce_Level_1", LoadSceneMode.Single);
     }
 }
