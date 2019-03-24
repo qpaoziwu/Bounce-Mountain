3acbc7c80d36f63f22518e52dfcbaa78c07be5f4
LEOLEOW <48531116+LEOLEOW@users.noreply.github.com>
2019-03-24 07:33:26 -0400

    Booster
    Added in booster prefab. Adjustable boostamount for booster

---
+++ Assets/Bounce Mountain/Scripts/PowerUpBooster.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpBooster : MonoBehaviour
+{
+    [Range(0.0f, 50.0f)] public float boostAmount; 
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.attachedRigidbody.AddForce(boostAmount * other.attachedRigidbody.velocity, ForceMode2D.Impulse);
+        }
+
+    }
+}
