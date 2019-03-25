e1f3571a95692b552995d66e9978ee2cedd7c7b9
LEOLEOW <48531116+LEOLEOW@users.noreply.github.com>
2019-03-24 22:24:26 -0400

    Added Directions for booster
    Dropdown menu in inspector allows player to change the direction of the booster.
    
    -"None" option will boost the player in the direction theyre moving in

--- Assets/Bounce Mountain/Scripts/PowerUpBooster.cs
+++ Assets/Bounce Mountain/Scripts/PowerUpBooster.cs
@@ -4,14 +4,57 @@ using UnityEngine;
 
 public class PowerUpBooster : MonoBehaviour
 {
-    [Range(0.0f, 50.0f)] public float boostAmount; 
+    [Range(0.0f, 100.0f)] public float boostAmount;
+
+    //Dropdown menu for booster direction in inspector
+    public enum BoosterDirection { None, Up, Right, Down, Left }
+    public BoosterDirection boosterDirection;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.tag == "Player")
+        //If "None" is selected for booster direction
+        if (boosterDirection == BoosterDirection.None)
         {
-            other.attachedRigidbody.AddForce(boostAmount * other.attachedRigidbody.velocity, ForceMode2D.Impulse);
+            if (other.gameObject.tag == "Player")
+            {
+                other.attachedRigidbody.AddForce(boostAmount * other.attachedRigidbody.velocity, ForceMode2D.Impulse);
+            }
         }
 
+        //If "Up" is selected for booster direction
+        if (boosterDirection == BoosterDirection.Up)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                other.attachedRigidbody.AddForce(Vector2.up * boostAmount + other.attachedRigidbody.velocity, ForceMode2D.Impulse);
+            }
+        }
+
+        //If "Right" is selected for booster direction
+        if (boosterDirection == BoosterDirection.Right)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                other.attachedRigidbody.AddForce(Vector2.right * boostAmount + other.attachedRigidbody.velocity, ForceMode2D.Impulse);
+            }
+        }
+
+        //If "Down" is selected for booster direction
+        if (boosterDirection == BoosterDirection.Down)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                other.attachedRigidbody.AddForce(Vector2.down * boostAmount + other.attachedRigidbody.velocity, ForceMode2D.Impulse);
+            }
+        }
+
+        //If "Left" is selected for booster direction
+        if (boosterDirection == BoosterDirection.Left)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                other.attachedRigidbody.AddForce(Vector2.left * boostAmount + other.attachedRigidbody.velocity, ForceMode2D.Impulse);
+            }
+        }
     }
 }
