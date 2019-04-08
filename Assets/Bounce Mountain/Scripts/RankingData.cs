380adb2e84118361cc3c3cb01ce34dcb3dffa63b
SinisterCid <47873004+SinisterCid@users.noreply.github.com>
2019-04-08 15:17:57 -0400

    Added Ranking System

---
+++ Assets/Bounce Mountain/Scripts/RankingData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu]
+public class RankingData : ScriptableObject
+{
+    public float[] goldScoreTimes = new float[SceneManager.sceneCountInBuildSettings];
+    public float[] silverScoreTimes = new float[SceneManager.sceneCountInBuildSettings];
+    public float[] bronzeScoreTimes = new float[SceneManager.sceneCountInBuildSettings];
+}
--- Assets/Bounce Mountain/Scripts/StatsMenu.cs
+++ Assets/Bounce Mountain/Scripts/StatsMenu.cs
@@ -16,8 +16,26 @@ public class StatsMenu : MonoBehaviour
         for (int i = 0; i < levelTimes.Length; i++)
         {
             levelTimes[i].GetComponent<TextMeshProUGUI>().text = "Level " + (i + 1) + ": " + StatsManagerScript.highScoreTime[i + 1] + " s";
+            if(StatsManagerScript.highScoreTime[i+1] < StatsManagerScript.goldScoreTimes[i])
+            {
+                //Display Gold
+                Debug.Log(i + "Got Gold");
+            }
+            else if(StatsManagerScript.highScoreTime[i + 1] < StatsManagerScript.silverScoreTimes[i])
+            {
+                //Display Silver
+                Debug.Log(i + "Got Silver");
+            }
+            else if(StatsManagerScript.highScoreTime[i + 1] < StatsManagerScript.bronzeScoreTimes[i])
+            {
+                //Display Bronze
+                Debug.Log(i + "Got Bronze");
+            }
+            else
+            {
+                //Display empty metal
+                Debug.Log(i + "Got Nothing");
+            }
         }
-
-
     }
 }
