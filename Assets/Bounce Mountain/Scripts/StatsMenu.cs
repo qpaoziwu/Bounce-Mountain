bcc446a662c319d64c3d969714da528c11ee0a09
SinisterCid <47873004+SinisterCid@users.noreply.github.com>
2019-04-08 16:00:33 -0400

    Crowns all work with the ranking system

--- Assets/Bounce Mountain/Scripts/StatsMenu.cs
+++ Assets/Bounce Mountain/Scripts/StatsMenu.cs
@@ -1,40 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 
 public class StatsMenu : MonoBehaviour
 {
     public GameObject[] levelTimes;
+    public GameObject[] rankingCrown;
+    public Sprite[] rankingCrownSprites = new Sprite[4];
+    
     GameObject StatsManagerObject;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        
         StatsManager StatsManagerScript = GameObject.FindGameObjectWithTag("StatsManager").GetComponent<StatsManager>();
         for (int i = 0; i < levelTimes.Length; i++)
         {
             levelTimes[i].GetComponent<TextMeshProUGUI>().text = "Level " + (i + 1) + ":       " + StatsManagerScript.highScoreTime[i + 1] + " s";
+
             if(StatsManagerScript.highScoreTime[i+1] < StatsManagerScript.goldScoreTimes[i] && StatsManagerScript.highScoreTime[i + 1] != 0)
             {
-                //Display Gold
-                Debug.Log((i+1) + "Got Gold");
+                rankingCrown[i].GetComponent<Image>().sprite = rankingCrownSprites[0];
             }
             else if(StatsManagerScript.highScoreTime[i + 1] < StatsManagerScript.silverScoreTimes[i] && StatsManagerScript.highScoreTime[i + 1] != 0)
             {
-                //Display Silver
-                Debug.Log((i+1) + "Got Silver");
+                rankingCrown[i].GetComponent<Image>().sprite = rankingCrownSprites[1];
             }
             else if(StatsManagerScript.highScoreTime[i + 1] < StatsManagerScript.bronzeScoreTimes[i] && StatsManagerScript.highScoreTime[i + 1] != 0)
             {
-                //Display Bronze
-                Debug.Log((i+1) + "Got Bronze");
+                rankingCrown[i].GetComponent<Image>().sprite = rankingCrownSprites[2];
             }
             else
             {
-                //Display empty metal
-                Debug.Log((i+1) + "Got Nothing");
+                rankingCrown[i].GetComponent<Image>().sprite = rankingCrownSprites[3];
             }
         }
     }
