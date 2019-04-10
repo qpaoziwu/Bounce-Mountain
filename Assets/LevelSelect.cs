22b16344fd081e5b53008f02b3121a8e40357301
DrewGodden <35704776+DrewGodden@users.noreply.github.com>
2019-04-10 02:18:01 -0400

    Added functionality to level select screen

---
+++ Assets/LevelSelect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public int level;
+
+    public void SelectLevel()
+    {
+        SceneManager.LoadScene(level);
+    }
+}
