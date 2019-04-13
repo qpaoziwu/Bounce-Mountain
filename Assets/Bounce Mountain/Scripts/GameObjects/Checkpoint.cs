22fa479bef1acf6572313b735649aa27ff86ff14
DrewGodden <35704776+DrewGodden@users.noreply.github.com>
2019-04-13 04:16:49 -0400

    Cleaned up checkpoint code

--- Assets/Bounce Mountain/Scripts/GameObjects/Checkpoint.cs
+++ Assets/Bounce Mountain/Scripts/GameObjects/Checkpoint.cs
@@ -12,43 +12,13 @@ public class Checkpoint : MonoBehaviour
     public OnHitEvent OnHit = new OnHitEvent();
     private bool hit = false;
 
-    /* Checkpoint time code - removed due to being obsolete
-    public int checkpointNumber;
-    public Text checkpointTimeText;
-    private float timeFromStart;
-    private float checkpointReachedTime;
-
-    
-    // Update is called once per frame
-    void Update()
-    {
-        timeFromStart = Time.time;
-    }
-    */
-
     private void OnTriggerEnter2D(Collider2D trigger)
     {
         if (trigger.gameObject.tag == "Player" && hit == false)
         {
-            //if (trigger.GetComponent<PlayerController>().currentCheckPoint == checkpointNumber - 1)
-            //{
             gameObject.GetComponent<SpriteRenderer>().color = Color.green;
             OnHit.Invoke();
-            Debug.Log("OnHit invoked!");
             hit = true;
-
-            /* Checkpoint time code - removed due to being obsolete
-            checkpointReachedTime = timeFromStart - trigger.GetComponent<PlayerController>().lastCheckpointTime;
-
-            if (checkpointTimeText != null)
-            {
-                checkpointTimeText.text = "Checkpoint " + checkpointNumber + ": " + checkpointReachedTime;
-            }
-
-            trigger.GetComponent<PlayerController>().lastCheckpointTime += checkpointReachedTime;
-            trigger.GetComponent<PlayerController>().currentCheckPoint = checkpointNumber;
-            */
-            //}
         }
     }
 }
